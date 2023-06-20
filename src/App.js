935b52530b864326d5f5aaf135ca4b1ad26d97dc
John Luu <122046486+TwinSkylar@users.noreply.github.com>
2023-06-20 13:27:04 -0400

    Static navbar at the top with a some styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Shopping from "./components/Shopping";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Shopping from "./components/Store";
 import Cart from "./components/Cart";
 
 function App() {
   return (
-    <Router>
+    <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shopping" element={<Shopping />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/store" element={<Shopping />} />
+        <Route path="/checkout" element={<Cart />} />
         <Route path="/nav" element={<Header />} />
         <Route path="*" element={<NoMatch />} />
-
       </Routes>
-    </Router>
+    </>
   );
 }
-
 
 function NoMatch() {
   return (
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 describe("Welcome Page", () => {
-  test('Displays all menu items', () => {
-    render(<App/>);
-    expect(screen.getByText('Home')).toBeInTheDocument();
-    expect (screen.getByText('Pokemon Store')).toBeInTheDocument();
-    expect (screen.getByText('Checkout')).toBeInTheDocument();
-    expect (screen.getByText('Store')).toBeInTheDocument();
+  test("Displays all menu items", () => {
+    render(<App />);
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Pokemon Store")).toBeInTheDocument();
+    expect(screen.getByText("Checkout")).toBeInTheDocument();
+    expect(screen.getByText("Store")).toBeInTheDocument();
   });
 
-  test('Displays the home page', () => {
+  test("Displays the home page", () => {
     render(<App />);
-    expect(screen.getByText('Go Shopping')).toBeInTheDocument();
+    expect(screen.getByText("Go Shopping")).toBeInTheDocument();
   });
 });
 
+describe("Tests all the routing", () => {
+  test("should redirect and update history", () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    userEvent.click(screen.getByText(/Home/));
+    expect(history.location.pathname).toEqual("/home");
+  });
+
+  test('should redirect and update dom', () => {
+    render(<BrowserRouter><App/></BrowserRouter>);
+
+    userEvent.click(screen.getByText(/Home/));
+
+    expect(screen.getByText('Go Shopping')).toBeInTheDocument();
+});
+});

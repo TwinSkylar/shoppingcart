9fd7a14f9abaf50a3b2cdd155b26065a7194c655
John Luu <122046486+TwinSkylar@users.noreply.github.com>
2023-06-20 10:50:38 -0400

    Added initial React routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Shopping from "./components/Shopping";
+import Cart from "./components/Cart";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shopping" element={<Shopping />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/nav" element={<Header />} />
+        <Route path="*" element={<NoMatch />} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>404: Page Not Found</h2>
+      <p>There are no pokemon in this region</p>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+describe("Welcome Page", () => {
+  test('Displays all menu items', () => {
+    render(<App/>);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect (screen.getByText('Pokemon Store')).toBeInTheDocument();
+    expect (screen.getByText('Checkout')).toBeInTheDocument();
+    expect (screen.getByText('Store')).toBeInTheDocument();
+  });
+
+  test('Displays the home page', () => {
+    render(<App />);
+    expect(screen.getByText('Go Shopping')).toBeInTheDocument();
+  });
 });
+
